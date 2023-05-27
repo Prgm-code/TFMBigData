5d2d24da77ff745c0f1efd6dbfd5eaa90c7b521a
prgm-code <patriocio@prgmdev.com>
2023-05-27 09:41:03 +0000

    cambio a venv

--- jupyterhub-instalation.sh
+++ jupyterhub-instalation.sh
@@ -1,60 +1,70 @@
 #!/bin/bash
 set -e
+sudo apt-get install python3.10-venv -y
+# Comprobar si Python 3 está instalado
+command -v python3 >/dev/null 2>&1 || {
+  echo >&2 "Python 3 no está instalado. Instalando...";
+  sudo apt-get update;
+  sudo apt-get install -y python3;
+}
+
+# Comprobar si pip está instalado
+command -v pip3 >/dev/null 2>&1 || {
+  echo >&2 "pip no está instalado. Instalando...";
+  sudo apt-get install -y python3-pip;
+}
+
+# Comprobar si npm está instalado
+command -v npm >/dev/null 2>&1 || {
+  echo >&2 "npm no está instalado. Instalando...";
+  sudo apt-get install -y npm;
+}
 
 # Directorios y rutas
 JUPYTER_DIR="/opt/jupyterhub"
-JUPYTER_ENV_DIR="$JUPYTER_DIR/miniconda/envs/jupyterhub-env"
+JUPYTER_ENV_DIR="$JUPYTER_DIR/env"
 JUPYTER_CONFIG_DIR="$JUPYTER_DIR/.jupyterhub"
 JUPYTER_LOG_DIR="$JUPYTER_DIR/log"
-MINICONDA_INSTALLER="$JUPYTER_DIR/miniconda.sh"
-MINICONDA_BIN_DIR="$JUPYTER_DIR/miniconda/bin"
 
 # Crea los directorios necesarios
-sudo mkdir -p $JUPYTER_CONFIG_DIR $JUPYTER_LOG_DIR
-sudo chmod 777 $JUPYTER_CONFIG_DIR $JUPYTER_LOG_DIR
+sudo mkdir -p $JUPYTER_CONFIG_DIR $JUPYTER_LOG_DIR $JUPYTER_ENV_DIR
+sudo chmod 777 $JUPYTER_CONFIG_DIR $JUPYTER_LOG_DIR $JUPYTER_ENV_DIR
 
 # Genera la secret key
 openssl rand -hex 32 | sudo tee $JUPYTER_CONFIG_DIR/jupyterhub_cookie_secret
 sudo chmod 400 $JUPYTER_CONFIG_DIR/jupyterhub_cookie_secret
 cd $JUPYTER_DIR
-# Descarga e instala miniconda
-sudo curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o $MINICONDA_INSTALLER
-if [[ ! -f $MINICONDA_INSTALLER ]]; then
-  echo "La descarga de Miniconda ha fallado"
-  exit 1
+
+# Crea un entorno virtual de Python
+sudo python3 -m venv $JUPYTER_ENV_DIR
+
+# Activa el entorno virtual de Python
+source $JUPYTER_ENV_DIR/bin/activate
+
+# Actualiza pip
+sudo pip install --upgrade pip
+
+## otorga permisos de escritura a pip
+sudo chmod 777 -R $JUPYTER_ENV_DIR
+
+
+# Cambia el directorio de instalación global de npm a un directorio en tu directorio de inicio
+if [[ ! -d ~/.npm-global ]]; then
+    mkdir ~/.npm-global
+    npm config set prefix '~/.npm-global'
+    echo 'export PATH=~/.npm-global/bin:$PATH' >> ~/.profile
+    source ~/.profile
 fi
-sudo bash $MINICONDA_INSTALLER -b -p $JUPYTER_DIR/miniconda
-if [[ ! -d $MINICONDA_BIN_DIR ]]; then
-  echo "La instalación de Miniconda ha fallado"
-  exit 1
-fi
-sudo chown -R $USER:$USER $JUPYTER_DIR/miniconda
-sudo chmod -R 777 $JUPYTER_DIR/miniconda
 
-
-sudo rm $MINICONDA_INSTALLER
-
-# Añade conda al PATH
-echo 'export PATH="'$MINICONDA_BIN_DIR':$PATH"' >> ~/.bashrc
-source ~/.bashrc
-
-# Configura conda
-$MINICONDA_BIN_DIR/conda config --set always_yes yes --set changeps1 no
-$MINICONDA_BIN_DIR/conda update -q conda
-
-# Crea el entorno de conda
-$MINICONDA_BIN_DIR/conda create -p $JUPYTER_ENV_DIR python=3.8
-
-# Instala los paquetes necesarios
-$MINICONDA_BIN_DIR/conda install -p $JUPYTER_ENV_DIR -c conda-forge jupyterhub jupyterlab jupyter-hdfscm -y
-
-# Instala PySpark
-$MINICONDA_BIN_DIR/conda install -p $JUPYTER_ENV_DIR -c conda-forge pyspark
+# Instala JupyterHub y proxy
+pip install jupyterhub jupyterlab
+npm install -g configurable-http-proxy
 
 # Inicia JupyterHub
-$JUPYTER_ENV_DIR/bin/jupyterhub --generate-config
-sudo mv jupyterhub_config.py $JUPYTER_DIR/
+sudo touch $JUPYTER_DIR/jupyterhub_config.py
 
+
+sudo chmod 777 $JUPYTER_DIR/jupyterhub_config.py
 
 sudo echo "c.JupyterHub.bind_url = 'http://:9909'" >> $JUPYTER_DIR/jupyterhub_config.py
 sudo echo "c.JupyterHub.cookie_secret_file = '/opt/jupyterhub/.jupyterhub/jupyterhub_cookie_secret'" >> $JUPYTER_DIR/jupyterhub_config.py
@@ -64,8 +74,6 @@ sudo echo "c.Spawner.default_url = '/lab'" >> $JUPYTER_DIR/jupyterhub_config.py
 sudo echo "c.Authenticator.admin_users = {'hadoop', 'jupyterhub'}" >> $JUPYTER_DIR/jupyterhub_config.py
 sudo echo "c.LocalAuthenticator.create_system_users = True" >> $JUPYTER_DIR/jupyterhub_config.py
 sudo echo "c.JupyterHub.hub_ip = ''" >> $JUPYTER_DIR/jupyterhub_config.py
-
-
 
 # Configura JupyterHub como un servicio
 echo "[Unit]
